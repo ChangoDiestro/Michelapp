6b58cbba0d8c2ed1e982b560ba594202a6ce4642
Gerardo <ferrari_gerardo@live.com.mx>
2018-10-20 13:34:12 -0500

    UIAlert Added and Tablew View Action

--- Michelapp/Michelapp/Controllers/ListBeerViewController.swift
+++ Michelapp/Michelapp/Controllers/ListBeerViewController.swift
@@ -8,7 +8,38 @@
 
 import UIKit
 
-class ListBeerViewController: UIViewController {
+class ListBeerViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+        
+        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+ 
+        
+        let favorite = UITableViewRowAction(style: .normal, title: "Favorite") { action, index in
+            print("favorite button tapped")
+            let alert = UIAlertController(title: "Congrutulations", message: "You addes a favorites beers", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        favorite.backgroundColor = .orange
+        
+ 
+        
+        return [favorite]
+    }
+   
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,14 +53,6 @@ class ListBeerViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
