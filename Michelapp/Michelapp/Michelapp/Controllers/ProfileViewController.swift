bd2df5168d71251928de3fd95310e010191a6c85
tiradoJ <31317599+tiradoJ@users.noreply.github.com>
2018-10-26 19:43:26 -0500

    add Profile menu
    se agrega el perfil

---
+++ Michelapp/Michelapp/Michelapp/Controllers/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  Michelapp
+//
+//  Created by Jesús Tirado on 10/26/18.
+//  Copyright © 2018 Abraham Quezada. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
